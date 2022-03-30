4be07056f080cfb65fee93680c3e2ad22cae308b
Megan Martel <mmartel1330@gmail.com>
2022-03-30 12:11:10 -0400

    code cleanup

--- index.js
+++ index.js
@@ -13,22 +13,15 @@ const db = getFirestore();
 
 const custRef = db.collection('customers')
 
-// added each customer then commented out for the rest of the code 
-// custRef.add(customers[])
-// .then(doc => {
-//     console.log('Added customer', doc.id);
-// })
-// .catch(err => {
-//     console.error(err)
-// });
 
-custRef.doc('IGmkedlJ2L73CfcV4bA0').get() //this pulls one doc
+custRef.doc('IGmkedlJ2L73CfcV4bA0').get() 
 .then(doc => {
     console.log(doc.id, '=>', doc.data());
 })
 .catch(err => console.error(err));
 
-custRef.get() //this pulls all docs in collection
+
+custRef.get() 
 .then(snapshot => {
     snapshot.forEach(doc => {
     console.log(doc.id, '=>', doc.data());
@@ -36,6 +29,6 @@ custRef.get() //this pulls all docs in collection
 })
 .catch(console.error);
 
-//update a document 
+
 custRef.doc('dYNpzFFstNdntXQ11rmm').update({email:'megan@gmail.com'})
 
